9268b6b831d2234a4b27d09e6496c2922bd3316b
wistox07 <fodelosrios@gmail.com>
2024-05-06 07:15:50 -0500

    Implementando retrofit en Fragment

--- app/src/main/java/com/wistox07/movieapp/data/Api.kt
+++ app/src/main/java/com/wistox07/movieapp/data/Api.kt
@@ -5,8 +5,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.POST
-
-class Api {
+//object es singleton
+object Api {
     private val buider: Retrofit.Builder = Retrofit.Builder()
         .baseUrl("http://192.168.18.5")
         .addConverterFactory(GsonConverterFactory.create())
--- app/src/main/java/com/wistox07/movieapp/presentation/login/LoginFragment.kt
+++ app/src/main/java/com/wistox07/movieapp/presentation/login/LoginFragment.kt
@@ -1,19 +1,58 @@
 package com.wistox07.movieapp.presentation.login
 
 import android.os.Bundle
+import android.telecom.Call
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.wistox07.movieapp.R
+import com.wistox07.movieapp.data.Api
+import com.wistox07.movieapp.data.LoginDto
+import com.wistox07.movieapp.data.LoginRequest
+import com.wistox07.movieapp.databinding.FragmentLoginBinding
+import retrofit2.Callback
+import retrofit2.Response
 
 class LoginFragment : Fragment() {
 
+    private lateinit var binding : FragmentLoginBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentLoginBinding.bind(view)
+
+        binding.btnSingIn.setOnClickListener {
+            val email = binding.edtEmail.text.toString()
+            val password = binding.edtPassword.text.toString()
+
+            val request = Api.build().singIn(LoginRequest(email, password))
+            request.enqueue(object : Callback<LoginDto> {
+                override fun onResponse(call: retrofit2.Call<LoginDto>, response: Response<LoginDto>){
+                    if(response.isSuccessful){
+                        val loginDto = response.body()
+                        loginDto?.let {
+                            println(it.data.user)
+                        }
+                    }else{
+                        println(response.message())
+                    }
+                }
+
+                override fun onFailure(call: retrofit2.Call<LoginDto>, t: Throwable) {
+                    println(t.message)
+                }
+
+            })
+
+
+        }
     }
 }
